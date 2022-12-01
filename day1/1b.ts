c98382a613f2eec045f6d7cb25b0c0cc5a786f1f
FitzHawke <will@fitzhawk.com>
2022-12-01 01:36:19 -0500

    advent of code 2022 day 1 solution b

---
+++ day1/1b.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs'
+
+const input = readFileSync('./input').toString()
+
+let arr = input.split('\n')
+
+let totals:number[] = [];
+let count = 0;
+
+for(let i = 0; i < arr.length; i++) {
+    if (arr[i] === '') {
+        totals.push(count);
+        count = 0;
+    } else {
+        count += Number(arr[i])
+    }
+}
+totals.push(count)
+totals.sort((a,b)=>b-a)
+console.log(totals[0]+totals[1]+totals[2])
