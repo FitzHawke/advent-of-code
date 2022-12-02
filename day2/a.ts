7f10e570530afd032607856864cba4bbadebbdc9
FitzHawke <will@fitzhawk.com>
2022-12-02 01:15:58 -0500

    day 2 part 1

---
+++ day2/a.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("./input").toString();
+
+let hash = new Map()
+  .set("A", 1)
+  .set("B", 2)
+  .set("C", 3)
+  .set("X", 1)
+  .set("Y", 2)
+  .set("Z", 3);
+
+let games = input.split("\n");
+let score = 0;
+
+for (let game of games) {
+  let matchScore = hash.get(game[2]) - hash.get(game[0]);
+  score += hash.get(game[2]);
+
+  if (matchScore === 0) score += 3;
+  else if (matchScore === 1 || matchScore === -2) score += 6;
+}
+console.log(score);
