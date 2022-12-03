8b8e2cb5e42dd8cf0551ab8859fce723fde53e3c
FitzHawke <will@fitzhawk.com>
2022-12-03 04:04:36 -0500

    day 3 part 1

---
+++ day3/a.ts
@@ -0,0 +1,28 @@
+import { Hash } from "crypto";
+import { readFileSync } from "fs";
+
+const input = readFileSync("./input").toString().split("\n");
+
+const getPrio = (s:string):number => {
+  if (s.toLocaleUpperCase() === s) {
+    return s.charCodeAt(0) - 38;
+  } else {
+    return s.charCodeAt(0) - 96;
+  }
+}
+
+let total = 0;
+
+for (let pack of input) {
+  let items = new Set()
+  for(let i = 0; i<pack.length; i++) {
+    if (i<pack.length / 2) {
+      items.add(pack[i])
+    } else if (items.has(pack[i])){
+      total += getPrio(pack[i])
+      break;
+    }
+  }
+}
+
+console.log(total)
