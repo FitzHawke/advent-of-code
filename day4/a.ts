d53f07f895a219b3b1d84bdb6dbf54c55d597e75
FitzHawke <will@fitzhawk.com>
2022-12-04 00:36:48 -0500

    day 4 part 1

---
+++ day4/a.ts
@@ -0,0 +1,18 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("./input").toString().split("\n");
+
+let count = 0;
+
+for (let i of input) {
+  let [start1, mid, end2] = i.split("-");
+  let [end1, start2] = mid.split(",");
+
+  if (
+    (+start1 <= +start2 && +end1 >= +end2) ||
+    (+start2 <= +start1 && +end2 >= +end1)
+  ) {
+    count++;
+  }
+}
+console.log(count);
