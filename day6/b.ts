3b3af02e505d674f0416020f6cd1a9f633da19dc
FitzHawke <will@fitzhawk.com>
2022-12-06 00:20:11 -0500

    day 6 part 1

--- day6/b.ts
+++ day6/b.ts
@@ -1,3 +1,25 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("./input").toString();
+
+let last13:string[] = new Array().fill("", 14)
+let count = 0;
+
+const check = (last3:string[], l:string):boolean => {
+  let [l1,l2,l3] = last3
+  return (l !== l1 && l !== l2 && l !== l3 && l1 !== l2 && l1 !== l3 && l2 !== l3 && l1 !== "")
+}
+
+for (let l of input) {
+  count++;
+  
+  console.log(last13,l)
+  if (check(last13, l)) {
+    break;
+  } else {
+    last13.shift()
+    last13.push(l)
+  }
+}
+
+console.log(count);
