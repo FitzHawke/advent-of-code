f4d685c5845efa9735f6f773f5c981fc6363efa4
FitzHawke <will@fitzhawk.com>
2022-12-08 02:20:13 -0500

    day 8 part b

--- day8/partA.ts
+++ day8/partA.ts
@@ -3,18 +3,24 @@ const input = readFileSync("./input.txt").toString();
 
 const trees = input.split("\n");
 let count = (trees.length + trees[0].length - 2) * 2;
-const dirs: number[][] = [[-1,0], [1,0], [0,1], [0,-1]]
+const dirs: number[][] = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
 
-const isVisible = (x:number, y:number, t: number):boolean => {
+const isVisible = (y: number, x: number, t: number): boolean => {
   let maxX = trees[0].length;
   let maxY = trees.length;
 
   for (const dir of dirs) {
-    let curr: number[] = [x, y]
+    let curr: number[] = [y, x];
     let visible = true;
-    while(visible) {
-      curr = [curr[0] + dir[0], curr[1] + dir[1]]
-      if (curr[0]<0 || curr[0]>=maxX || curr[1]<0 || curr[1]>=maxY) break;
+    while (visible) {
+      curr = [curr[0] + dir[0], curr[1] + dir[1]];
+      if (curr[0] < 0 || curr[0] >= maxY || curr[1] < 0 || curr[1] >= maxX)
+        break;
       if (Number(trees[curr[0]][curr[1]]) >= t) {
         visible = false;
       }
@@ -23,15 +29,15 @@ const isVisible = (x:number, y:number, t: number):boolean => {
   }
 
   return false;
-}
+};
 
 for (let i = 1; i < trees.length - 1; i++) {
   for (let j = 1; j < trees[0].length - 1; j++) {
-    const tree = Number(trees[i][j])
+    const tree = Number(trees[i][j]);
     if (isVisible(i, j, tree)) {
-      count++
+      count++;
     }
   }
 }
 
-console.log(count)
+console.log(count);
--- day8/partB.ts
+++ day8/partB.ts
@@ -1,3 +1,46 @@
 import { readFileSync } from "fs";
-
 const input = readFileSync("./input.txt").toString();
+
+const trees = input.split("\n");
+let count = (trees.length + trees[0].length - 2) * 2;
+const dirs: number[][] = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+const rating = (y: number, x: number, t: number): number => {
+  let maxX = trees[0].length;
+  let maxY = trees.length;
+  let rating = 1;
+
+  for (const dir of dirs) {
+    let count = 0;
+    let curr: number[] = [y, x];
+    let visible = true;
+    while (visible) {
+      curr = [curr[0] + dir[0], curr[1] + dir[1]];
+      if (curr[0] < 0 || curr[0] >= maxY || curr[1] < 0 || curr[1] >= maxX)
+        break;
+      count++;
+      if (Number(trees[curr[0]][curr[1]]) >= t) {
+        visible = false;
+      }
+    }
+    rating *= count;
+  }
+
+  return rating;
+};
+
+let maxRate = 0;
+
+for (let i = 0; i < trees.length; i++) {
+  for (let j = 0; j < trees[0].length; j++) {
+    const tree = Number(trees[i][j]);
+    maxRate = Math.max(rating(i,j,tree), maxRate)
+  }
+}
+
+console.log(maxRate);
