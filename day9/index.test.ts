3910de8d1494b767381d6bff4aa36eb36dd219d5
FitzHawke <will@fitzhawk.com>
2022-12-09 21:13:34 -0500

    convert day9 to use testing

--- day9/index.test.ts
+++ day9/index.test.ts
@@ -3,16 +3,18 @@ import { resolve } from "path";
 import partA from "./partA";
 import partB from "./partB";
 
-const day = "template"
+const day = "day9"
 const answers = {
-  "ae": 0,
-  "ai": 0,
-  "be": 0,
-  "bi": 0
+  "ae": 13,
+  "ai": 6236,
+  "be": 1,
+  "be2": 36,
+  "bi": 2449
 }
 
 let input = promises.readFile(resolve(process.cwd(), day, 'input.txt'), 'utf8');
 let example = promises.readFile(resolve(process.cwd(), day, 'example.txt'), 'utf8');
+let example2 = promises.readFile(resolve(process.cwd(), day, 'example2.txt'), 'utf8');
 
 describe('Part A', () => {
   it('should produce the correct value for example', async () => {
@@ -26,6 +28,9 @@ describe('Part A', () => {
 describe('Part B', () => {
   it('should produce the correct value for example', async () => {
     expect(partB(await example)).toEqual(answers.be)
+  })
+  it('should produce the correct value for example 2', async () => {
+    expect(partB(await example2)).toEqual(answers.be2)
   })
   it('should produce the correct value for input', async () => {
     expect(partB(await input)).toEqual(answers.bi)
