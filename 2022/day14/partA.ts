51e10a693de679e17e4227ad53b728ac6f05e7a5
FitzHawke <will@fitzhawk.com>
2022-12-15 15:09:29 -0500

    use stack in day 14 solutions

--- 2022/day14/partA.ts
+++ 2022/day14/partA.ts
@@ -1,6 +1,6 @@
 type space = '#' | 'o';
 export type mapCol = Map<number, space>;
-type coords = [number, number];
+export type coords = [number, number];
 
 const buildWall = (dir1: coords, dir2: coords, map: mapCol[]): mapCol[] => {
   const [x1, y1] = dir1,
@@ -41,17 +41,21 @@ export const buildSlice = (structures: string[]): mapCol[] => {
 const dropSand = (map: mapCol[]): number => {
   let sand = 0,
     done = false;
+  let stack = [[500, 0]] as coords[];
   while (!done) {
     sand++;
     let stopped = false;
-    let [x, y] = [500, 0];
+    let [x, y] = stack.pop() || [500, 0];
     while (!stopped) {
       if (!map[y + 1].has(x)) {
+        stack.push([x, y]);
         y += 1;
       } else if (!map[y + 1].has(x - 1)) {
+        stack.push([x, y]);
         y += 1;
         x -= 1;
       } else if (!map[y + 1].has(x + 1)) {
+        stack.push([x, y]);
         y += 1;
         x += 1;
       } else {
--- 2022/day14/partB.ts
+++ 2022/day14/partB.ts
@@ -1,19 +1,23 @@
-import { buildSlice, mapCol } from './partA';
+import { buildSlice, coords, mapCol } from './partA';
 
 const dropSand = (map: mapCol[]): number => {
   let sand = 0,
     done = false;
+  let stack = [[500, 0]] as coords[];
   while (!done) {
     sand++;
     let stopped = false;
-    let [x, y] = [500, 0];
+    let [x, y] = stack.pop() || [500, 0];
     while (!stopped) {
       if (!map[y + 1].has(x)) {
+        stack.push([x, y]);
         y += 1;
       } else if (!map[y + 1].has(x - 1)) {
+        stack.push([x, y]);
         y += 1;
         x -= 1;
       } else if (!map[y + 1].has(x + 1)) {
+        stack.push([x, y]);
         y += 1;
         x += 1;
       } else {
