60bb41b171cd0152f1eb886410faf881681e663a
FitzHawke <will@fitzhawk.com>
2022-12-18 16:07:29 -0500

    optimized solution for day 16 part a

---
+++ 2022/day16/index.test.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect } from 'vitest';
+import { promises } from 'fs';
+import { resolve } from 'path';
+import partA from './partA';
+import partB from './partB';
+
+const year = '2022';
+const day = 'day16';
+const answers = {
+  ae: 1651,
+  ai: 1850,
+  be: 1707,
+  bi: 2306,
+};
+
+let input = promises.readFile(
+  resolve(process.cwd(), year, day, 'input.txt'),
+  'utf8',
+);
+let example = promises.readFile(
+  resolve(process.cwd(), year, day, 'example.txt'),
+  'utf8',
+);
+
+describe(`${year}-${day}-Part A`, () => {
+  it('should produce the correct value for example', async () => {
+    expect(partA(await example, 30)).toEqual(answers.ae);
+  });
+  it('should produce the correct value for input', async () => {
+    expect(partA(await input, 30)).toEqual(answers.ai);
+  });
+});
+
+describe(`${year}-${day}-Part B`, () => {
+  it.skip('should produce the correct value for example', async () => {
+    expect(partB(await example, 26)).toEqual(answers.be);
+  });
+  it.skip('should produce the correct value for input', async () => {
+    expect(partB(await input, 26)).toEqual(answers.bi);
+  });
+});
---
+++ 2022/day16/partA.ts
@@ -0,0 +1,90 @@
+type Valves = {
+  [key: string]: {
+    rate: number;
+    exits: string[];
+  };
+};
+type Dists = {
+  [key: string]: number;
+};
+export type DVI = {
+  dists: Dists;
+  impt: string[];
+  valves?: Valves;
+};
+
+const getDistances = (valves: Valves): DVI => {
+  const imptValves = Object.keys(valves).filter(
+    (v) => valves[v].rate > 0 || v === 'AA',
+  );
+  const dists = {} as Dists;
+
+  for (const start in valves) {
+    if (!imptValves.includes(start)) continue;
+    let cur = new Set([start]) as Set<string>;
+    let next = new Set() as Set<string>;
+    let dist = 0;
+    dists[[start, start].join('-')] = 0;
+    while (cur.size) {
+      dist++;
+      cur.forEach((loc) => {
+        for (const loc2 of valves[loc].exits) {
+          if (!dists.hasOwnProperty([start, loc2].join('-'))) {
+            dists[[start, '-', loc2].join('')] = dist;
+            next.add(loc2);
+          }
+        }
+      });
+      cur = next;
+      next = new Set() as Set<string>;
+    }
+  }
+
+  return { dists, impt: imptValves };
+};
+
+export const parseInputs = (inputs: string[]): DVI => {
+  const valves: Valves = {};
+  inputs.map((v) => {
+    let [left, right] = v.split('; ');
+    const rate = Number(left.split('=').at(-1));
+    const id = left.split(' ')[1];
+    const exits = right.replaceAll(',', '').split(' ').slice(4);
+    valves[id] = { rate, exits };
+  });
+  return { valves, ...getDistances(valves) };
+};
+
+const main = (input: string, time: number): number => {
+  const { valves, dists, impt } = parseInputs(input.split('\n'));
+
+  const findBest = (
+    cur: string,
+    time: number,
+    seen: Set<string>,
+    important: string[],
+  ): number => {
+    seen.add(cur);
+    important = important.filter((i) => !seen.has(i));
+    let best = 0;
+
+    for (const im of important) {
+      const timeLeft = time - dists[[cur, im].join('-')] - 1;
+      if (timeLeft >= 0) {
+        let flow = valves![im].rate * timeLeft;
+        flow += findBest(im, timeLeft, new Set(...seen), [...important]);
+        if (flow > best) best = flow;
+      }
+    }
+    return best;
+  };
+  return findBest('AA', time, new Set(), impt);
+};
+
+export default function (input: string, time: number): number {
+  console.log('\nDay 16: Proboscidea Volcanium\nPart A');
+  const startTime = new Date();
+  const result = main(input, time);
+  console.log(`Time elapsed: ${new Date().valueOf() - startTime.valueOf()}ms`);
+  return result;
+}
