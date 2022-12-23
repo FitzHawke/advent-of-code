47799ba7586105494acf700d1d8ef7ea30c26853
FitzHawke <will@fitzhawk.com>
2022-12-23 02:20:29 -0500

    day 23 solutions

---
+++ 2022/day23/index.test.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect } from 'vitest';
+import { promises } from 'fs';
+import { resolve } from 'path';
+import partA from './partA';
+import partB from './partB';
+
+const year = '2022';
+const day = 'day23';
+const answers = {
+  ae: 110,
+  ai: 3800,
+  be: 20,
+  bi: 916,
+};
+
+let input = promises.readFile(
+  resolve(process.cwd(), year, day, 'input.txt'),
+  'utf8',
+);
+let example = promises.readFile(
+  resolve(process.cwd(), year, day, 'example.txt'),
+  'utf8',
+);
+
+describe(`${year}-${day}-Part A`, () => {
+  it('should produce the correct value for example', async () => {
+    expect(partA(await example)).toEqual(answers.ae);
+  });
+  it('should produce the correct value for input', async () => {
+    expect(partA(await input)).toEqual(answers.ai);
+  });
+});
+
+describe(`${year}-${day}-Part B`, () => {
+  it('should produce the correct value for example', async () => {
+    expect(partB(await example)).toEqual(answers.be);
+  });
+  it('should produce the correct value for input', async () => {
+    expect(partB(await input)).toEqual(answers.bi);
+  });
+});
---
+++ 2022/day23/partB.ts
@@ -0,0 +1,27 @@
+import { Direction, makeMoves, parseIntoMap, proposeMoves } from './partA';
+
+const main = (input: string): number => {
+  const moveDirs: Direction[] = ['N', 'S', 'W', 'E'];
+  const elves = parseIntoMap(input);
+  let iters = 0;
+  let done = false;
+  while (!done) {
+    iters++;
+    const proposed = proposeMoves(elves, moveDirs);
+    if (proposed.size === 0) {
+      done = true;
+      break;
+    }
+    makeMoves(elves, proposed);
+    moveDirs.push(moveDirs.shift()!);
+  }
+  return iters;
+};
+
+export default function (input: string): number {
+  console.log('\nDay 23: Unstable Diffusion\nPart B');
+  const startTime = new Date();
+  const result = main(input);
+  console.log(`Time elapsed: ${new Date().valueOf() - startTime.valueOf()}ms`);
+  return result;
+}
