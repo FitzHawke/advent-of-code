04f28735ebde80ee28d8083ee88796e76744bf65
FitzHawke <will@fitzhawk.com>
2023-12-01 16:24:50 -0500

    2023 day 1 part A

---
+++ 2023/day01/partA.ts
@@ -0,0 +1,35 @@
+export const parseInputs = (inputs: string): string[] => {
+	return inputs.trim().split('\n');
+};
+
+const findNumbers = (lines: string[]): number[] => {
+	const lineNumbers: number[] = [];
+	lines.map((curLine) => {
+		let first = 0,
+			last = 0;
+		for (const char of curLine) {
+			if (!isNaN(+char)) {
+				last = +char;
+				if (first === 0) first = +char;
+			}
+		}
+		lineNumbers.push(first*10+last);
+	});
+	console.log(lineNumbers);
+	return lineNumbers;
+};
+
+const main = (input: string): number => {
+	return findNumbers(parseInputs(input)).reduce(
+		(acc, cur) => (acc += cur),
+		0,
+	);
+};
+
+export default function (input: string): number {
+	console.log('\nDay 01: Title\nPart A');
+	const startTime = new Date();
+	const result = main(input);
+	console.log(`Time elapsed: ${new Date().valueOf() - startTime.valueOf()}ms`);
+	return result;
+}
