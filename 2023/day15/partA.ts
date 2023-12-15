4fd9b229bb8e0a62c7f21d3b5819b9757762e21f
FitzHawke <will@fitzhawk.com>
2023-12-15 00:38:34 -0500

    2023 day 15 part a solution

---
+++ 2023/day15/partA.ts
@@ -0,0 +1,25 @@
+export const parseInput = (input:string): string[] => {
+	return input.split(',')
+}
+
+const calculateHash = (seq:string):number => {
+	let curVal = 0;
+	for (let i = 0; i < seq.length; i++) {
+		console.log(curVal,seq[i].charCodeAt(0), seq, i)
+		curVal = (curVal + seq[i].charCodeAt(0)) * 17 % 256
+	}
+	return curVal
+}
+
+const main = (input: string): number => {
+	const initSequence = (parseInput(input))
+	return initSequence.reduce((acc,cur)=> acc + calculateHash(cur),0);
+};
+
+export default function (input: string): number {
+	console.log('\nDay 15: Title\nPart A');
+	const startTime = new Date();
+	const result = main(input);
+	console.log(`Time elapsed: ${new Date().valueOf() - startTime.valueOf()}ms`);
+	return result;
+}
