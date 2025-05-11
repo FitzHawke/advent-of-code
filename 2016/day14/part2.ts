347b13e2b32e2ffb8e5d09097f8ff3fd6fb9f31d
FitzHawke <will@fitzhawk.com>
2025-05-11 05:11:26 -0400

    2016 day 14 reduce code duplication

--- 2016/day14/part2.ts
+++ 2016/day14/part2.ts
@@ -1,59 +1,16 @@
-import { md5Hash, parseInput } from './part1.ts';
+import { find64Key, md5Hash, parseInput } from './part1.ts';
 
-const find64Key = (salt: string): number => {
-	const seen = new Map<string, string>();
-	const keys: string[] = [];
-	let i = -1;
-
-	const reHash = (hash: string): string => {
-		let curHash = hash;
+const main = (input: string): number => {
+	const salt = parseInput(input);
+	const hashFunc = (str: string): string => {
+		let curHash = md5Hash(str);
 		for (let j = 0; j < 2016; j++) {
 			curHash = md5Hash(curHash);
 		}
 		return curHash;
 	};
 
-	const verifyKey = (val: string, iter: number) => {
-		for (let j = 1; j <= 1000; j++) {
-			const tstSalt = `${salt}${j + iter}`;
-			if (!seen.has(tstSalt)) seen.set(tstSalt, reHash(md5Hash(tstSalt)));
-
-			const hashed = seen.get(tstSalt);
-			const testStr = `${val}${val}${val}${val}${val}`;
-
-			if (hashed.includes(testStr)) {
-				keys.push(hashed);
-				return true;
-			}
-		}
-		return false;
-	};
-
-	while (keys.length < 64) {
-		i++;
-		const newSalt = `${salt}${i}`;
-
-		if (!seen.has(newSalt)) seen.set(newSalt, reHash(md5Hash(newSalt)));
-
-		const hashed = seen.get(newSalt);
-		const found: string[] = [];
-		for (let j = 2; j < hashed.length; j++) {
-			if (hashed[j] === hashed[j - 1] && hashed[j - 1] === hashed[j - 2]) {
-				found.push(hashed[j]);
-				break;
-			}
-		}
-		for (const f of found) {
-			if (verifyKey(f, i)) break;
-		}
-	}
-
-	return i;
-};
-
-const main = (input: string): number => {
-	const salt = parseInput(input);
-	return find64Key(salt);
+	return find64Key(salt, hashFunc);
 };
 
 export default function (input: string, title: string): number {
